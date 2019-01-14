cef64d6c67d748e17cd8b0a9f7d4b7b45098613b
Yuko <yuko.mazurek@gmail.com>
2019-01-13 23:52:31 -0800

    again

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
-var io = require("socket.io").listen(PORT);
+const io = require("socket.io").listen(PORT);
 
 io.on("connection", socket => {
   console.log("new connection", socket.id);
@@ -23,11 +31,3 @@ io.on("connection", socket => {
     io.emit("chat", msg);
   });
 });
-
-const path = require("path");
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
-  });
-}
