ecb292d7ac7bd88e0e836bffcf8e618a173ad624
Yuko <yuko.mazurek@gmail.com>
2019-01-13 23:29:48 -0800

    edited server.js for production

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const app = express();
-var io = require("socket.io").listen(5000);
 
-app.use(express.static("client/build"));
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/client/build/index.html");
-});
+const PORT = process.env.PORT || 5000;
+var io = require("socket.io").listen(PORT);
 
 io.on("connection", socket => {
   console.log("new connection", socket.id);
@@ -26,3 +23,11 @@ io.on("connection", socket => {
     io.emit("chat", msg);
   });
 });
+
+const path = require("path");
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+}
